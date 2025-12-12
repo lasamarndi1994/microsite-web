e58c0031ceba62d891156eb845c645b6f0a0dc45
lasaaliceblue <lasa@aliceblueindia.com>
2025-12-12 11:59:45 +0530

    Update the changes

--- src/components/AuthCard.vue
+++ src/components/AuthCard.vue
@@ -1,43 +1,47 @@
 <template>
   <v-card :elevation="$vuetify.display.smAndDown ? 0 : elevation" class="mx-auto my-2 auth-card"
-    :class="$vuetify.display.smAndDown ? 'pa-4 bg-transparent' : 'pa-16'"
-    :width="$vuetify.display.smAndDown ? '100%' : width" :min-height="$vuetify.display.smAndDown ? 'auto' : height">
+    :class="$vuetify.display.smAndDown ? 'pa-4 bg-transparent' : 'pa-16'">
     <slot></slot>
   </v-card>
 </template>
 
 <script setup>
-defineProps({
-  width: { type: String, default: "650px" },
-  height: { type: String, default: "380px" },
-  elevation: { type: Number, default: 5 },
-});
+// defineProps({
+//   width: { type: String, default: "650px" },
+//   height: { type: String, default: "380px" },
+//   elevation: { type: Number, default: 5 },
+// });
 </script>
 
 <style scoped>
 .auth-card {
+  width: 650px !important;
+}
 
-  /* Standard Laptop (960px - 1400px) */
-  @media (min-width: 960px) and (max-width: 1400px) {
+/* Standard Laptop (960px - 1400px) */
+/* @media (min-width: 960px) and (max-width: 1400px) {
     width: 500px !important;
     min-height: auto !important;
-  }
+  } */
 
-  /* Short Vertical Screens (Max Height 850px) */
-  @media (max-height: 850px) and (min-width: 960px) {
+/* Short Vertical Screens (Max Height 850px) */
+/* @media (max-height: 850px) and (min-width: 960px) {
     width: 500px !important;
     min-height: auto !important;
-  }
+  } */
 
-  /* Ultra-Short Screens (Max Height 600px) e.g. 1265x551 */
-  @media (max-height: 600px) {
-    padding: 50px 24px 24px !important;
+/* Ultra-Short Screens (Max Height 600px) e.g. 1265x551 */
+@media (max-width: 767px) {
+  .auth-card {
+    /* padding: 50px 24px 24px !important; */
     /* increased vertical padding again */
     width: 100% !important;
-    max-width: 650px !important;
+    /* max-width: 650px !important;
     margin-top: 4px !important;
     margin-bottom: 8px !important;
-    min-height: auto !important;
+    min-height: auto !important; */
   }
 }
+
+/* } */
 </style>
